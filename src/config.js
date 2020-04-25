ae747f918a06cec3879c19efe2ff03a878456d4a
Neves <marcos.neves@gmail.com>
2020-04-25 06:16:47 -0300

    Add github tokens

--- src/config.js
+++ src/config.js
@@ -1,29 +1,30 @@
 export const config = {
   // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
-  githubConvertedToken: "MzM2N2YxZDRjNDk5MmQ4MGVjNzQwYmM5Y2FlZWYyNDE0ZGQ4NWFiYQ==",
+  githubConvertedToken:
+    "ZDVhMTVjZDM1N2M0YTdmNDljNTAwN2Y1YTBhNTQzMjEyMTBlZDQyYwo=",
 
   // Your Github UserName
-  githubUserName: "saadpasta",
+  githubUserName: "neves",
 
   // Your Github Repo Name Where You Have your issues as Blog
-  githubRepo: "react-blog-github",
+  githubRepo: "neves.github.io",
 
   // Set it to true if you have a Github app to add to this project
   // and fill the client ID & secret
   enableOAuth: true,
-  OAuthClientID: 'Iv1.9f40fd53257d4102',
-  OAuthSecret: 'cc3db76ffe9848c92d83172807eececa79f0eb22',
+  OAuthClientID: "Iv1.8e43d288fde6494b",
+  OAuthSecret: "63ebac59b371efb08a23377276ddb840aaae3f8b",
 
   // Your Personal Blog Title
-  title : "Saad Pasta" ,
+  title: "AutoMagicaMente",
 
   // Your Personal Blog Subtitle
-  subtitle : "Software Developer",
+  subtitle: "Software Developer",
 
   // Header customization
   header: {
-    backgroundColor: '#f1f6f8', // can be a CSS gradient
-    titleColor: '#ff5252',
-    subtitleColor: '#37474f',
+    backgroundColor: "#f1f6f8", // can be a CSS gradient
+    titleColor: "#ff5252",
+    subtitleColor: "#37474f",
   },
-};
+}
