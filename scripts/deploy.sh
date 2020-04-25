7abb9a9a80cac35daa267f328b022e2188ad38c6
Neves <marcos.neves@gmail.com>
2020-04-25 14:09:07 -0300

    Log executed commands during deploy and back to branch dev at end

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/env sh
+set -x
 git checkout master || git checkout --orphan master
 git rm -rf .
 mv build/* build/.* .
 git add -A .
 git commit -m "$(date +'Blog updated at %d/%m/%y %H:%M')"
 git push --set-upstream origin master
+git checkout dev
